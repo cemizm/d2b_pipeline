e24dcb49e6d3d154378c2766c8622601fbed2715
Cem Basoglu <cem.basoglu@web.de>
2019-09-08 08:10:18 +0200

    WIP: Prepare

--- prepare.py
+++ prepare.py
@@ -1,92 +1,108 @@
 import pandas as pd
-import pvserve
-import sys
+import os
+import matplotlib.pyplot as plt
 
-prep_data = pvserve.smartmonitoring.PrepareData()
+from pvserve import mlcycle as ml
+from pvserve import smartmonitoring as sm
 
-meta_file = "res/meta_data.csv"
-meta_data = pd.read_csv(meta_file)
+COLS_U = ['Uoc', *sm.SM_CURVE_U]
+COLS_I = ['Isc', *sm.SM_CURVE_I]
+COLS_ERROR = ["plant_name", "string_name", "Anzahl Module in Serie", "Uoc_meta", "Isc_meta"]
 
-bright_file = "res/bright_curves.csv"
-bright_curves = pd.read_csv(bright_file)
-bright_curves = prep_data.cut_curves(bright_curves)
+ISC_UOC_TOLERANCE = 1.1
 
-dark_file = "res/dark_curves.csv"
-dark_curves = pd.read_csv(dark_file)
+INTERPOLATE_X = 20
 
-dark_x_bright = pd.merge(bright_curves, dark_curves, how="inner", on='parent_id')
-dark_x_bright.to_csv("res/dark_x_bright.csv")
-xxxx = pd.merge(dark_x_bright, meta_data, left_on='parent_id', right_on='string_id')
-xxxx.to_csv("res/xxxx.csv")
+print("------------------ Download from MLCycle -------------------")
 
-columns=["plant_id","string_id","temp", "E eff","Isc","Uoc","dark_max_x", "bright_max_x","dark_0","dark_1","dark_2","dark_3","dark_4",
-    "dark_5","dark_6","dark_7","dark_8","dark_9","dark_10","dark_11","dark_12",
-    "dark_13","dark_14","bright_0","bright_1","bright_2","bright_3","bright_4",
-    "bright_5","bright_6","bright_7","bright_8","bright_9","bright_10","bright_11","bright_12",
-    "bright_13","bright_14"]
+client_ml = ml.Client()
 
-dataframe = pd.DataFrame(columns=columns)
+meta = client_ml.download_dataframe("raw_metadata.csv", index_col=[0, 1])
+dark = client_ml.download_dataframe("raw_dark.csv", index_col=[0, 1])
+bright = client_ml.download_dataframe("raw_bright.csv", index_col=[0, 1])
 
-for index, row in xxxx.iterrows():
-    bright_x, bright_y = prep_data.get_x_y_data(row, "_x")
-    dark_x, dark_y = prep_data.get_x_y_data(row, "_y")
-    if len(bright_x) > 10 and len(dark_x) > 10:
-        bright_scala, bright_y = prep_data.get_interpolated_data_arrays(bright_x, bright_y, 15)
-        dark_scala, dark_y = prep_data.get_interpolated_data_arrays(dark_x, dark_y, 15)
-        dict = {
-            "plant_id" : row["plant_id"],
-            "string_id" : row["string_id"],
-            "temp" : row["T mod"],
-            "E eff" : row["E eff"],
-            "Isc" : row["Isc 0"],
-            "Uoc" : row["Uoc 0"],
-            "dark_max_x" : dark_x[-1],
-            "bright_max_x" : bright_x[-1],
-            "dark_0" : dark_y[0],
-            "dark_1" : dark_y[1],
-            "dark_2" : dark_y[2],
-            "dark_3" : dark_y[3],
-            "dark_4" : dark_y[4],
-            "dark_5" : dark_y[5],
-            "dark_6" : dark_y[6],
-            "dark_7" : dark_y[7],
-            "dark_8" : dark_y[8],
-            "dark_9" : dark_y[9],
-            "dark_10" : dark_y[10],
-            "dark_11" : dark_y[11],
-            "dark_12" : dark_y[12],
-            "dark_13" : dark_y[13],
-            "dark_14" : dark_y[14],
-            "bright_0" : bright_y[0],
-            "bright_1" : bright_y[1],
-            "bright_2" : bright_y[2],
-            "bright_3" : bright_y[3],
-            "bright_4" : bright_y[4],
-            "bright_5" : bright_y[5],
-            "bright_6" : bright_y[6],
-            "bright_7" : bright_y[7],
-            "bright_8" : bright_y[8],
-            "bright_9" : bright_y[9],
-            "bright_10" : bright_y[10],
-            "bright_11" : bright_y[11],
-            "bright_12" : bright_y[12],
-            "bright_13" : bright_y[13],
-            "bright_14" : bright_y[14],
-        }
-        dataframe = dataframe.append(dict, ignore_index=True)
+dark_count = len(dark.index)
+bright_count = len(bright.index)
 
-dataframe.to_csv("res/trainingsdata.csv")
+print("Bright IV-Curves:\t\t{}".format(bright_count))
+print("Dark IV-Curves:\t\t\t{}".format(dark_count))
+
+### Prepare data
+
+meta["Uoc_tol"] = meta.Uoc * ISC_UOC_TOLERANCE
+meta["Isc_tol"] = meta.Isc * ISC_UOC_TOLERANCE
+
+dark_meta = dark.join(meta, rsuffix="_meta")
+bright_meta = bright.join(meta, rsuffix="_meta")
+
+print("-------------------- Sanity Check Data ---------------------")
+
+dark_meta[COLS_U] = dark_meta[COLS_U].divide(dark_meta["Anzahl Module in Serie"], axis="index")
+bright_meta[COLS_U] = bright_meta[COLS_U].divide(bright_meta["Anzahl Module in Serie"], axis="index")
+
+berror = bright_meta.Uoc_meta.isnull() 
+berror = berror | bright_meta.Isc_meta.isnull()
+berror = berror | (bright_meta.Uoc > bright_meta.Uoc_tol) 
+berror = berror | (bright_meta.Isc > bright_meta.Isc_tol)
+berror = berror | (bright_meta.Uoc < 0)
+berror = berror | (bright_meta.Isc < 0)
+
+drop_bright = berror
+
+berror = bright_meta[berror][COLS_ERROR]
+berror = berror.reset_index().drop(columns='data_id')
+berror = berror.drop_duplicates().reset_index(drop=True)
+
+derror = dark_meta.Uoc_meta.isnull() 
+derror = derror | dark_meta.Isc_meta.isnull()
+derror = derror | (dark_meta.Uoc > dark_meta.Uoc_tol)
+derror = derror | (dark_meta.Isc > dark_meta.Isc_tol)
+
+drop_dark = derror
+
+derror = dark_meta[derror][COLS_ERROR]
+derror = derror.reset_index().drop(columns='data_id')
+derror = derror.drop_duplicates().reset_index(drop=True)
+
+drop_dark_len = len(drop_dark[drop_dark == True])
+drop_bright_len = len(drop_bright[drop_bright == True])
 
 
-'''
-Trainingsdaten:
-    15 I Werte der Dunkelkennlinie
-    Temp der Ziel Hellkennlinie
-    Einstrahlungsstärke der Ziel Hellkennlinie
+if drop_bright_len > 0:
+    print("Bright IV-Curve Errors:\t\t{}".format(drop_bright_len))
+    client_ml.upload_dataframe(berror, 'Errors Bright', 'error_bright.csv', 0)
 
-Zieldaten:
-    15 I Werte der Hellkennlinie
-    Isc kurzschlussstrom
-    Uoc leerlaufspannung
+    bright_meta = bright_meta.drop(bright_meta[drop_bright].index)
 
-'''
+if drop_dark_len > 0:
+    print("Dark IV-Curves Errors:\t\t{}".format(drop_dark_len))
+    client_ml.upload_dataframe(berror, 'Errors Dark', 'error_dark.csv', 0)
+    
+    dark_meta = dark_meta.drop(dark_meta[drop_dark].index)
+
+print("--------------------- Normalize Data -----------------------")
+
+dark_meta[COLS_U] = dark_meta[COLS_U].divide(dark_meta.Uoc_tol, axis="index")
+dark_meta[COLS_I] = dark_meta[COLS_I].divide(dark_meta.Isc_tol, axis="index")
+
+bright_meta[COLS_U] = bright_meta[COLS_U].divide(bright_meta.Uoc_tol, axis="index")
+bright_meta[COLS_I] = bright_meta[COLS_I].divide(bright_meta.Isc_tol, axis="index")
+
+fig, ax = plt.subplots(figsize=(18, 10))
+
+ax.scatter(bright_meta['E eff'], 
+           (bright_meta['Uoc'] * bright_meta['Isc']))
+
+ax.set_xlabel('Einstrahlung')
+ax.set_ylabel('Watt @ MPP (normalisiert)')
+
+client_ml.upload_plot(fig, "Data Distribution", "dd_bright.png", 0)
+
+fig, ax = plt.subplots(figsize=(18, 10))
+
+ax.scatter(bright_meta['Uoc'], bright_meta['Isc'])
+
+ax.set_xlabel('Uoc (normalisiert)')
+ax.set_ylabel('Isc (normalisiert)')
+
+client_ml.upload_plot(fig, "Data Distribution (Dunkel)", "dd_dark.png", 0)
